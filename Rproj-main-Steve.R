674e9fc3c44fc779314d79bee763705b05dbad26
smorty <smortensen@outlook.com>
2017-08-01 09:31:53 -0400

    Updated - left joins instead of merge

--- Rproj-main-Steve.R
+++ Rproj-main-Steve.R
@@ -122,14 +122,16 @@ zip_codes <- read_csv("zipcodes.csv"
                                     ,"Location","Decommisioned"))
 
 # Merging all files together - 
-# R automatically recognizes the primary key by matching column names.
-master <- merge(merge(merge(reviews, movies),reviewers),zip_codes)
+# Using left join to maintain movies as our base table, row count before and after matches.
+master1 <- left_join(reviews, reviewers, by="reviewer_id")
+master2 <- left_join(master1, movies, by="movie_id")
+master <- left_join(master2, zip_codes, by="zip_code")
 
 # 6. 
-master$totGenres <- rowSums(master[,c(11:29)])
-genresPercent <- nrow(master[master$totGenres > 1,])/nrow(master)
+master$totGenres <- rowSums(master[,c(14:32)])
+genresPercent <- nrow(master[master$totGenres > 1,])/nrow(master) * 100
 genresPercent
-# 69.95066%
+# 69.938%
 
 # 7. 
 fRvwrs <- master[master$gender == "F",]
@@ -142,15 +144,18 @@ t.int.conf.test <- function(set, count, conf.level) {
   return(c(mean(set) - abs(qt((1-conf.level)/2, count-1)) * (sd(set)/sqrt(count)), mean(set) + abs(qt((1-conf.level)/2, count-1)) * (sd(set)/sqrt(count))))
 }
 
-fRtngConf <- t.int.conf.test(femRvwrs$rating, nrow(femRvwrs), 0.95)
+fRtngConf <- t.int.conf.test(fRvwrs$rating, nrow(fRvwrs), 0.95)
 fRtngConf
-# 3.526243 3.555151
+# 3.517202 3.545813
 
 mRtngConf <- t.int.conf.test(mRvwrs$rating, nrow(mRvwrs), 0.95)
 mRtngConf
-# 3.524903 3.541300
+# 3.521309 3.537269
 
 # 8. 
+master$State[grepl("[A-Z]",master$zip_code)] <- 'Canada'
+master$State[is.na(master$State)] <- 'unknown'
+
 by_region <- group_by(master, State)
 
 countRegion <- summarise(by_region, totRvw = length(rating))
@@ -178,24 +183,24 @@ movieNumNums <- movieNumNums[order(-totNum),]
 movieNumNums
 #    totRvw totNum  percent
 #     <int>  <int>    <dbl>
-#  1      1    148 8.851675
-#  2      2     73 4.366029
-#  3      3     65 3.887560
-#  4      5     57 3.409091
-#  5      4     53 3.169856
-#  6      7     38 2.272727
-#  7      6     36 2.153110
-#  8      9     36 2.153110
-#  9     10     32 1.913876
-# 10      8     31 1.854067
-# ... with 246 more rows
+#  1      1    141 8.382878
+#  2      2     68 4.042806
+#  3      4     64 3.804994
+#  4      3     60 3.567182
+#  5      5     51 3.032105
+#  6      7     44 2.615933
+#  7      6     39 2.318668
+#  8      9     33 1.961950
+#  9     10     33 1.961950
+# 10      8     30 1.783591
+# ... with 263 more rows
 
 # 10.
 meanGenre = data.frame(Genre = NA, MeanRating = NA)
 
 j <- 0
 
-for (i in 11:29) {
+for (i in 14:32) {
   print(i)
   meanGenre[j,] <- c(colnames(master[i]), mean(master$rating[master[,i]==1]))
   j <- j+1
@@ -208,11 +213,11 @@ meanGenre
 hiAvgRvw <- tail(meanGenre, 1)
 hiAvgRvw
 #     Genre       MeanRating
-# Film-Noir 3.93195625759417
+# Film-Noir 3.92152336987882
 loAvgRvw <- head(meanGenre, 1)
 loAvgRvw
 #   Genre       MeanRating
-# Fantasy 3.21648690292758
+# Fantasy 3.21523668639053
 
 # 11.
 oldMeanGenre = data.frame(Genre = NA, MeanRating = NA)
@@ -221,7 +226,7 @@ oldMaster <- master[master$age > 30,]
 
 j <- 0
 
-for (i in 11:29) {
+for (i in 14:32) {
   print(i)
   oldMeanGenre[j,] <- c(colnames(oldMaster[i]), mean(oldMaster$rating[oldMaster[,i]==1]))
   j <- j+1
@@ -234,11 +239,11 @@ oldMeanGenre
 hiAvgRvw <- tail(oldMeanGenre, 1)
 hiAvgRvw
 #     Genre       MeanRating
-# Film-Noir 4.00411099691675
+# Film-Noir 3.99805447470817
 loAvgRvw <- head(oldMeanGenre, 1)
 loAvgRvw
 #  Genre      MeanRating
-# Horror 3.2824644549763
+# Horror 3.28520017993702
 
 yngMeanGenre = data.frame(Genre = NA, MeanRating = NA)
 
@@ -246,7 +251,7 @@ yngMaster <- master[master$age <= 30,]
 
 j <- 0
 
-for (i in 11:29) {
+for (i in 14:32) {
   print(i)
   yngMeanGenre[j,] <- c(colnames(yngMaster[i]), mean(yngMaster$rating[yngMaster[,i]==1]))
   j <- j+1
@@ -259,8 +264,8 @@ yngMeanGenre
 hiAvgRvw <- tail(yngMeanGenre, 1)
 hiAvgRvw
 #     Genre       MeanRating
-# Film-Noir 3.82763744427935
+# Film-Noir 3.80992907801418
 loAvgRvw <- head(yngMeanGenre, 1)
 loAvgRvw
 #   Genre       MeanRating
-# Fantasy 3.08507670850767
+# Fantasy 3.09078590785908

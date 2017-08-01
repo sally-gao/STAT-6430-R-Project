d9d03fbd46b93c0e6ceb15894edb380763a6f544
Kennan Grant <kennangrant@gmail.com>
2017-08-01 02:40:24 -0400

    substantially revised analysis

--- Rproj-main-Kennan.R
+++ Rproj-main-Kennan.R
@@ -22,142 +22,147 @@ normalized_base_table <- reviews %>%
   left_join(zip_codes, by = "zip_code")
 
 
-modes::modes(normalized_base_table$rating)[1]
+# The Fundamental Question ------------------------------------------------
 
-# Univariate Analysis -----------------------------------------------------
-
-# reviwer_id
+# 
+# Given an explanatory variable value/level is an attribute of an observation, 
+# what is the probability that the response variable of the observation is
+# a particular value/level? If the probability is higher than the probability
+# of encountering that response value/level in the overall dataset, then that
+# explanatory variable value/level is predictive and can be said to be
+# "associated" with the target response value/level.
 #
-# mode rating of reviewer
+# In our case, we want to know 'what seems to be associated with a high rating?'
+# In other words: What variable values/levels, when present, predict a high
+# rating with a higher probability than the baseline probability in the overall
+# dataset?
+#
+
+# Rating Distribution Overview (Graph 1) --------------------------------------
+
+# rating distribution bar chart
+normalized_base_table %>% 
+  ggplot(aes(x = rating)) +
+  geom_bar(aes(y = (..count..)/sum(..count..))) + 
+  scale_y_continuous(labels = scales::percent) +
+  ylab(label = "Percent")
+
+# bar chart data, in table
+normalized_base_table %>% 
+  group_by(rating) %>% 
+  summarise(n = n()
+            , percent = n/100000*100)
+
+# conclusion: 
+34.174 + 21.201 # 55.375% of reviews are 'high' ratings of 4 or 5. This is the 
+# 'baseline probability'.  For a variable value/level to be 
+# 'associated' with a high-rating (4 or 5), then, given that particular
+# variable value/level the probability of a high-rating
+# must be higher than 55.375%. 
+
+
+
+# Reviewer Breakdown By Mode Rating (Graph 2) ---------------------------------
+
+# find mode rating of reviewer
 reviewer_id_ratings <- normalized_base_table %>% 
   group_by(reviewer_id) %>% 
   summarise(mode_rating = modes::modes(rating)[1]) %>% 
   arrange(reviewer_id)
 
-mode_appended <- normalized_base_table %>% # append reviewer mode to ratings
+# append reviewer mode col to a dataframe
+mode_appended <- normalized_base_table %>% 
   select(reviewer_id, rating) %>% 
   left_join(reviewer_id_ratings, by = "reviewer_id")
 
+# # calculate probability of high rating, given high mode rating
+high_mode_ratings <- mode_appended %>% 
+  group_by(mode_rating == 4 | mode_rating == 5) %>% 
+  summarise(n = n()
+            , perc_high_rating = sum(rating == 4 | rating == 5)/n*100) %>% 
+  as.data.frame() %>% 
+  arrange(desc(perc_high_rating)) # 64% -- implies association
 
-modes::modes(normalized_base_table$rating)[1] # mode overall is '4'
-sum(normalized_base_table$rating == 4) / nrow(normalized_base_table) * 100
-# 34.1% of reviews are overall mode of '4'
-sum(mode_appended$rating == mode_appended$mode_rating) / 
-  nrow(mode_appended) * 100 # 41% of time, mode of reviewer predicts review
-# this is better than the 34.1% of reviews that are the overall mode of '4'.
-#
-# Note that we checked to make sure that it wasn't not the case that many
-# reviewers were not 1-review reviewers, which would have made our prediction
+# create pie chart of 64% !!!
+
+
+# Note that we checked to make sure that it was *NOT* the case that many
+# reviewers were 1-review reviewers, which would have made our prediction
 # technique useless going forward.  In fact, the minimum number of reviews
 # of any reviewer in this dataset is 20.
 
-# ---
 
-# timestamp
-#
+# Timestamp (Graph 3) ---------------------------------------------------------
+
+# calculate probability of high rating, given an hour of the timestamp
 normalized_base_table %>% 
-  group_by(lubridate::wday(timestamp)) %>% 
-  summarise(mean(rating)
-            , modes::modes(rating)[1]
-            , n = n()) # no signal for day of week
-
-normalized_base_table %>% 
-  group_by(lubridate::hour(timestamp)) %>% 
-  summarise(mean(rating)
-            , modes::modes(rating)[1]
-            , n = n()) %>% 
-  as.data.frame() # there is signal for hour of day.  
+  group_by(hour = lubridate::hour(timestamp)) %>% 
+  summarise(n = n()
+            , perc_high_rating = sum(rating == 4 | rating == 5)/n*100) %>% 
+  as.data.frame() %>% 
+  arrange(desc(perc_high_rating)) # hours 16, 15, and 12 are most likely to 
+# yield high rating (61.7%, 61.6%, and 60% -- respectively)
 
 
-# release_date
-#
+# Release Date ------------------------------------------------------------
+
 normalized_base_table$release_date <- as.Date(
   x=normalized_base_table$release_date,format='%d-%b-%Y') # convert to date
 
 normalized_base_table %>% 
   group_by(lubridate::month(release_date)) %>% 
-  summarise(mean(rating)
-            , modes::modes(rating)[1]
-            , n = n()) # August and May slightly more likely to be 3 than other
-                       # which are more likely to be 4
+  summarise(n = n()
+            , perc_high_rating = sum(rating == 4 | rating == 5)/n*100) %>% 
+  as.data.frame() %>% 
+  arrange(desc(perc_high_rating)) # only January is even mildly associated 
+# with high rating, at 58.3%
 
 
-# Fantasy
-normalized_base_table %>% 
-  group_by(Fantasy) %>% 
-  summarise(mean(rating)
-            , modes::modes(rating)[1]
-            , n = n()) # 3.21 avg-- lower
+# Genres -------------------------------------------------------------
 
 # Film-Noir   
 normalized_base_table %>% 
-  group_by(`Film-Noir`) %>% 
-  summarise(mean(rating)
-            , modes::modes(rating)[1]
-            , n = n()) # 3.92 avg-- higher
+  filter(`Film-Noir` == 1) %>% 
+  summarise(n = n()
+            , unique_reviewers = n_distinct(reviewer_id)
+            , perc_high_rating = sum(rating == 4 | rating == 5)/n*100) %>% 
+  as.data.frame() %>% 
+  arrange(desc(perc_high_rating)) # 70.6%
 
-# Horror
-normalized_base_table %>% 
-  group_by(Horror) %>% 
-  summarise(mean(rating)
-            , modes::modes(rating)[1]
-            , n = n()) # 3.29 avg-- lower
 
 # War    
 normalized_base_table %>% 
-  group_by(War) %>% 
-  summarise(mean(rating)
-            , modes::modes(rating)[1]
-            , n = n()) # 3.81 avg-- higher
+  filter(War == 1) %>% 
+  summarise(n = n()
+            , unique_reviewers = n_distinct(reviewer_id)
+            , perc_high_rating = sum(rating == 4 | rating == 5)/n*100) %>% 
+  as.data.frame() %>% 
+  arrange(desc(perc_high_rating)) # 66.5%
 
 
-# convert lat and long to double
-normalized_base_table <- normalized_base_table %>% 
-  mutate(Lat = as.double(Lat)
-         ,Long = as.double(Long))
 
+# State -------------------------------------------------------------------
 
-# gender
-#
-# shows distributions by rating by gender
+# State is *very* predictive.
 normalized_base_table %>% 
-  ggplot(aes(x=rating, fill = factor(gender))) +
-  geom_bar() +
-  facet_wrap(~ gender, scales = "free")
-
-normalized_base_table %>% 
-  group_by(gender, rating) %>% 
-  summarise(n = n()) %>% 
-  mutate(proportion = n / sum(n)) # only minor differences
-
-# occupation
-#
-# 
-normalized_base_table %>% 
-  group_by(occupation) %>% 
-  summarise(n = n(),
-            mean = mean(rating)) %>% 
-  arrange(desc(n)) %>% 
-  as.data.frame() # healthcare gives low -- 2.90
+  group_by(State) %>% 
+  summarise(n = n()
+            , unique_reviewers = n_distinct(reviewer_id)
+            , perc_high_rating = sum(rating == 4 | rating == 5)/n*100) %>% 
+  as.data.frame() %>% 
+  arrange(desc(perc_high_rating))
 
 
-# City
-#
-# 
+# City --------------------------------------------------------------------
+
+# City is very predictive, but a closer look reveals it is single reviewers
 normalized_base_table %>% 
   group_by(City) %>% 
-  summarise(n = n(),
-            mean = mean(rating)) %>% 
-  arrange(desc(n)) %>% 
-  as.data.frame() # NYC gives low -- 2.94
+  summarise(n = n()
+            , unique_reviewers = n_distinct(reviewer_id)
+            , perc_high_rating = sum(rating == 4 | rating == 5)/n*100) %>% 
+  #as.data.frame() %>% 
+  arrange(desc(perc_high_rating))
 
 
-
-
-# variable list -----------------------------------------------------------
-
-
-
-"Lat"           
-"Long"          
 
